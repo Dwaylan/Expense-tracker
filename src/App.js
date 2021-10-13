109cafeee2069a9b467045285fe2ac870a7a0444
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-10-13 12:44:40 -0400

    moved expense Item component folder to hold all of the files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import ExpenseItem from "./Components/ExpenseItem/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <div classname="Expense-container">
+      <div>
+        <h2>Date</h2>
+      </div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>
+        <h2>Amount</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
