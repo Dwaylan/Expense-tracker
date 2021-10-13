4c76bbffa4a2eac48a7db110fd443e3149084240
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-10-13 13:39:19 -0400

    added props

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -1,17 +1,17 @@
 import React from "react";
-import "./ExpenseItem.css"
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <div>
-        <h2>Date</h2>
+        <h2>{props.date.toISOstring()}</h2>
       </div>
       <div className="expense-item__description">
-        <h2>Title</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">
-        <h2>Amount</h2>
+        <h2>${props.amount}</h2>
       </div>
     </div>
   );
