d49a34192ce6c0bd64ccf1b03dc56f867e8f2e73
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-11-03 19:42:40 -0400

    cleaning out extra code, and change the fle head name

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Expense from "./Components/Expense/Expense";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
 const DUMMY_EXPENSES = [
-  { id: "e1", title: "Phone", amount: 150, date: new Date(2021, 10, 15) },
-  { id: "e2", title: "Car Note", amount: 365, date: new Date(2021, 10, 22) },
-  { id: "e3", title: "Gym", amount: 55, date: new Date(2021, 10, 28) },
+  {
+    id: "e1",
+    title: "Example Expense",
+    amount: 150,
+    date: new Date(2021, 10, 15),
+  },
 ];
 function App() {
   // The initial state is set to render the array of dummy expenses until setFiltered year
