caa64a599933470ced135cab32e69183a6d81aff
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-11-03 19:28:24 -0400

    project complete

--- src/Components/Chart/ChartBar.js
+++ src/Components/Chart/ChartBar.js
@@ -1,11 +1,12 @@
 import React from "react";
+
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
   let barFillHeight = "0%";
 
   if (props.maxValue > 0) {
-    barFillHeight = Math.round((props.value / props.maxValue) * 100 + "%");
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   return (
     <div className="chart-bar">
@@ -15,7 +16,7 @@ const ChartBar = (props) => {
           style={{ height: barFillHeight }}
         ></div>
       </div>
-      <div className="chart-bar__label"></div>
+      <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
